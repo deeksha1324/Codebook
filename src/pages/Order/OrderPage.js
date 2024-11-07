8b90a88be8f0b30c54c358555aaec18af692126a
deeksha <duddusmail@gmail.com>
2024-11-07 13:12:41 +0530

    committing new changes

--- src/pages/Order/OrderPage.js
+++ src/pages/Order/OrderPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import OrderSuccess from './components/OrderSuccess'
 import OrderFail from './components/OrderFail'
-import { useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
+import useTitle from '../../hook/useTitle';
 
 export default function OrderPage() {
-    const { state } = useLocation()
+    const { state } = useLocation();
+    useTitle("Order details")
     
   return (
     <main>
